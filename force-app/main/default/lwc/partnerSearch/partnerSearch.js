6be63364238902436e0be9aa5530e03d8c2e0076
Rajesh <rajesh@email.com>
2023-02-02 01:05:33 +0000

    Partner Search Changes

--- force-app/main/default/lwc/partnerSearch/partnerSearch.js
+++ force-app/main/default/lwc/partnerSearch/partnerSearch.js
@@ -1,23 +1,93 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc'; //Step 1: Import wire decorator 
 
+//Step 2: Import wire decorator > Import APEX method
+import fetchPartnerTypes from '@salesforce/apex/PartnerSearch.getAllPartnerTypes';
 
-export default class PartnerSearch extends LightningElement {
+import  { NavigationMixin} from 'lightning/navigation';
+
+export default class PartnerSearch extends NavigationMixin(LightningElement) {
 
     appdesc = 'Partner Management app helps Sales & Marketing Team to quickly find list of Channel partners having relationship with the organization. These partners have been vetted by Vendor relationship team. Sales & Marketing users can also use the app to share feedback about a partner and find other details like primary point of contact, location, Sale pipeline etc';
    
+    partnerTypes; // to hold all partners to bind in combobox
 
-    value = 'inProgress';
-
-    get options() {
-        return [
-            { label: 'New', value: 'new' },
-            { label: 'In Progress', value: 'inProgress' },
-            { label: 'Finished', value: 'finished' },
-        ];
-    }
-
+  
+  
     handleChange(event) {
-        this.value = event.detail.value;
+        
     }
+
+    //Step 3: Import wire decorator > Import APEX method > Make a call to the method
+    @wire(fetchPartnerTypes)
+    processOutput({data,error})
+    {
+        if(data)
+        {
+            console.log('Before - Partner Type::' + JSON.stringify(data));
+            this.partnerTypes = [{label:'--- Select Partner Type ---', value: ''}];
+
+            // loop the data and change object keys
+            data.forEach(item => {
+                
+                const partnerType = {};
+                partnerType.label = item.Name;
+                partnerType.value = item.Id;
+
+                this.partnerTypes.push(partnerType);
+                
+
+            }); 
+            console.log('After - Partner Type::' + JSON.stringify(this.partnerTypes));
+
+        }
+        else if(error)
+        {
+            console.log('Error:: ' + error.body.message);
+        }
+    }
+
+    OpenNewPartnerTypeStdPage()
+    {
+        // configure input param
+        const inputparm = {
+            type: 'standard__objectPage',
+            attributes: {
+                actionName: 'new',
+                objectApiName:'Partner_Type__c'
+            }
+        };
+
+        this[NavigationMixin.Navigate](inputparm);
+    }
+
+    OpenNewAccountStdPage()
+    {
+        // configure input param
+        const inputparm = {
+            type: 'standard__objectPage',
+            attributes: {
+                actionName: 'new',
+                objectApiName:'Account'
+            }
+        };
+
+        this[NavigationMixin.Navigate](inputparm);
+    }
+
+    OpenNewContactStdPage()
+    {
+        // configure input param
+        const inputparm = {
+            type: 'standard__objectPage',
+            attributes: {
+                actionName: 'new',
+                objectApiName:'Contact'
+            }
+        };
+
+        this[NavigationMixin.Navigate](inputparm);
+    }
+
+    
     
 }
