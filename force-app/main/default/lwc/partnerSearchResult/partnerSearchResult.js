ea6ed8a64266f40bc5168722f57fba0ba16b4d48
Rajesh <rajesh@email.com>
2023-02-04 01:11:39 +0000

    Partner search Result changes

--- force-app/main/default/lwc/partnerSearchResult/partnerSearchResult.js
+++ force-app/main/default/lwc/partnerSearchResult/partnerSearchResult.js
@@ -1,15 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 
+import fetchPartnerType from '@salesforce/apex/partnerSearchResultController.getPartners';
 
 export default class PartnerSearchResult extends LightningElement {
 
-    accountData = {
+    /*accountData = {
         Id: '0x111',
         userImg : 'https://randomuser.me/api/portraits/med/women/22.jpg',
         companyName : 'Oracle',
         userName: 'John Doe',
         partnerType:'Marketing PArtner'
-    };
+    };*/
 
+    channelPartnerTypeID = '';
+
+    partnerDataFromDB; // local property to hold all partner from DB
+
+    // Make a call to APEX method
+    @wire(fetchPartnerType,{partnerTypeId: '$channelPartnerTypeID'})
+    processOutput({data, error}) // Use {data, error} default system properties from result object to access retrieved data 
+    {
+        if(data)
+        {
+            console.log('Data from DB::' + JSON.stringify(data));
+            this.partnerDataFromDB = data; // copy the data
+
+
+        }
+        else  if(error)
+        {
+            console.log('Error');
+        }
+    }
+
+
+    get IsPartnerfound()
+    {
+        if(this.partnerDataFromDB != null && this.partnerDataFromDB.length > 0)
+        {
+            return true;
+        }
+        return false;
+    }
    
 }
