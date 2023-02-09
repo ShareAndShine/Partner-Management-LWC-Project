c4ba94ccf9fc409e919f408eb2ba380f120111ea
Rajesh <rajesh@email.com>
2023-02-09 01:34:27 +0000

    Added PArt II

--- force-app/main/default/lwc/partnerContactLocation/partnerContactLocation.js
+++ force-app/main/default/lwc/partnerContactLocation/partnerContactLocation.js
@@ -1,9 +1,66 @@
 import { LightningElement, api, wire } from 'lwc';
 
+import { getRecord } from 'lightning/uiRecordApi';
+
+// import leaftlet from static resource
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import LEAFLET from '@salesforce/resourceUrl/leaflet';
 
 export default class PartnerContactLocation extends LightningElement {
 
  
-    
+    // public property to receive partner account Id from parent component (partner Detail)
+    @api partnerAccountId; 
 
+    // property to hold location coordinates
+    latitude;
+    longitude;
+
+    leaftetMap;
+
+    // get lat and long values from DB using get Record
+    @wire(getRecord, { recordId: '$partnerAccountId', fields: ['Account.Partner_Geo_Location__Latitude__s', 'Account.Partner_Geo_Location__Longitude__s']})
+    processOutput({data,error})
+    {
+        if(data)
+        {
+            // Read values from field and assign to a local property
+            this.latitude = data.fields.Partner_Geo_Location__Latitude__s.value;
+            this.longitude = data.fields.Partner_Geo_Location__Longitude__s.value;
+            
+            console.log('lat:' + data.fields.Partner_Geo_Location__Latitude__s.value);
+            console.log('longt:' + data.fields.Partner_Geo_Location__Longitude__s.value);
+        }
+        else if(error)
+        {
+            console.log('Error');
+        }
+    }
+
+    connectedCallback()
+    {
+        Promise.all([
+            loadStyle(this, LEAFLET + '/leaflet.css'),
+            loadScript(this, LEAFLET + '/leaflet.js'),
+        ]).then(() => {
+            this.plotMap();
+        });
+    }
+
+    plotMap()
+    {
+        // find the div where we wanted to plot the map
+        const map = this.template.querySelector('.map');
+        if(map){
+            this.leafletMap = L.map(map, {zoomControl : true} ).setView([51.505, -0.09], 13);
+            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {attribution : 'Contact Location'}).addTo(this.leafletMap);
+        }
+
+        //const location = [51.505, -0.09];
+        const location = [this.latitude, this.longitude];
+
+        const leafletMarker = L.marker(location);
+        leafletMarker.addTo(this.leafletMap);
+        this.leafletMap.setView(location);
+    }
 }
--- force-app/main/default/lwc/partnerDetail/partnerDetail.js
+++ force-app/main/default/lwc/partnerDetail/partnerDetail.js
@@ -1,10 +1,73 @@
 import { LightningElement, wire } from 'lwc';
 
+import  { NavigationMixin} from 'lightning/navigation';
 
 
+import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
+import ACCOUNT_CONTRACT_START from '@salesforce/schema/Account.Partner_Contract_Start_Date__c';
 
-export default class PartnerDetail extends LightningElement {
+import PARTNER_PRIMARY_POC_FIELD from '@salesforce/schema/Account.Partner_Type_Lookup__c';
+import Partner_Budget__c_FIELD from '@salesforce/schema/Account.Partner_Budget__c';
+import Partner_Total_Sales_Revenue_FIELD from '@salesforce/schema/Account.Partner_Total_Sales_Revenue__c';
+import Partner_Active_Pipeline_Value_FIELD from '@salesforce/schema/Account.Partner_Active_Pipeline_Value__c';
+import Partner_Contract_End_Date__c_FIELD from '@salesforce/schema/Account.Partner_Contract_End_Date__c';
+import Number_of_trained_Partner_contacts__c_FIELD from '@salesforce/schema/Account.Number_of_trained_Partner_contacts__c';
+
+import Partner_Latitude_FIELD from '@salesforce/schema/Account.Partner_Geo_Location__Latitude__s';
+import Partner_Longitude_FIELD from '@salesforce/schema/Account.Partner_Geo_Location__Longitude__s';
+
+export default class PartnerDetail extends NavigationMixin(LightningElement) {
 
     
+    objectApi = 'Account';
+    selectedPartnerAccountId = '0015h00001CcXOBAA3'; //TODO: Remove hard coding
 
+    showLocation = false; // property to hold falg to open location component
+
+
+    // Expose property and map fields
+    // Bind the property in the template
+    accountName = ACCOUNT_NAME;
+    accContractStartDate = ACCOUNT_CONTRACT_START;
+    primarypoc = PARTNER_PRIMARY_POC_FIELD;
+    budget = Partner_Budget__c_FIELD;
+    salesRevenue = Partner_Total_Sales_Revenue_FIELD;
+    activepipeline = Partner_Active_Pipeline_Value_FIELD;
+    contractEnd = Partner_Contract_End_Date__c_FIELD;
+    totalTrained = Number_of_trained_Partner_contacts__c_FIELD;
+    partnerLongitude = Partner_Longitude_FIELD;
+    partnerLatitude = Partner_Latitude_FIELD;
+
+
+
+    get IsPartnerSelected()
+    {
+        if(this.selectedPartnerAccountId != null && this.selectedPartnerAccountId.length >0 )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    OpenPartnerReviewScreenFlow()
+    {
+        // use navigation mixin and redirect user to flow screen
+
+        const flowURL = '/flow/Rate_Partner_Performance?partnerAccountId=' + this.selectedPartnerAccountId;
+
+         // configure input param
+         const inputparm = {
+            type: 'standard__webPage',
+            attributes: {
+                url:'/flow/Rate_Partner_Performance?partnerAccountId=' + this.selectedPartnerAccountId
+            }
+        };
+
+        this[NavigationMixin.Navigate](inputparm);
+    }
+
+    OpenContactLocation()
+    {
+        this.showLocation = true;
+    }
 }
